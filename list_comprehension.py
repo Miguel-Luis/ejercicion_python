0630ce24e0d7f7886fa207ce7205eafb054d1973
Luis Miguel Gonzalez <miguel.gonzalez@cinnco.co>
2022-10-22 18:45:44 -0500

    Se agrega archivo de practica para List Comprehension aplicado a listas

---
+++ list_comprehension.py
@@ -0,0 +1,18 @@
+# --------------- Sintaxis tradicional -------
+numbers = []
+
+for element in range(1, 11):
+    numbers.append(element * 2)
+
+print(numbers)
+# --------------------------------------------
+
+# -------------- Sintaxis List Comprehension ------------
+numbers_v2 = [element * 2 for element in range(1, 11)]
+print(numbers_v2)
+# -------------------------------------------------------
+
+# -------------- Sintaxis List Comprehension con Condicional ------------
+numbers_v3 = [i * 2 for i in range(1, 11) if i % 2 == 0]
+print(numbers_v3)
+# -----------------------------------------------------------------------
